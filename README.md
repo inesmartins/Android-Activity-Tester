5ca9784b7d0ef5937eb0a6e60d9f074378f43601
Inês Martins <in.afmartins@gmail.com>
2021-12-29 11:26:46 +0000

    adds basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Android-Activity-Tester
-Enumerates all accessible activities and launch them one by one using adb.
+Enumerates all exported activities and launch them one by one using adb.
+
+According to the android developer docs, an activity is considered exported in the following two cases:
+
+* If the activity has `android:exported="true"` attribute in its definition.
+* If `android:exported` is not defined at all then it should have at least one intent-filter in it.
---
+++ activity_app_tester.py
@@ -0,0 +1,58 @@
+import os
+from xml.dom.minidom import parseString
+import argparse
+
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return arg
+
+def get_parsed_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--manifest', 
+                        dest="manifest",
+                        required=True,
+                        metavar="FILE", 
+    					type=lambda x: is_valid_file(parser, x),
+                        help='Path to the AndroidManifest.xml file')    
+    args = parser.parse_args()
+    return args
+
+def get_activities_dictionary(manifest_file_path):
+    data = ''
+    with open(manifest_file_path,'r') as f:
+        data = f.read()
+    dom = parseString(data)
+    activities = dom.getElementsByTagName('activity')
+    activities_dictionary = {}
+    for activity in activities:
+        activity_name = activity.toxml().split('android:name="')[1].split('"')[0]
+        activities_dictionary[activity_name] = 'android:exported="true"' in activity.toxml() or '<intent-filter>' in activity.toxml()
+    return activities_dictionary
+
+def get_adb_formatted_activity(activity_name):
+    if 'com.ingka.ikea.app' in activity_name:
+        return '.' + activity_name.split('com.ingka.ikea.app.')[1]
+    else:
+        return activity_name
+
+if __name__ == '__main__':
+    args = get_parsed_args()
+    dict = get_activities_dictionary(args.manifest)
+
+    print('\n=================== Exported =================== ')
+    for entry in dict:
+        if dict[entry] == True:
+            print(entry)
+
+    print('\n=================== Not Exported ===================')
+    for entry in dict:
+        if dict[entry] == False:
+            print(entry)
+
+    print('\n=================== ADB Test ===================')
+    for entry in dict:
+        if dict[entry] == True:
+            os.system('adb shell am start -n com.ingka.ikea.app/' + get_adb_formatted_activity(entry))
+            input("Press Enter to continue...")
